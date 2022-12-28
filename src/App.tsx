5c8a8c7e6ab75bef091c39c50ae692325a9d4a70
arricodyanto <arricohandyanto@gmail.com>
2022-12-28 08:31:43 +0700

    first deploy

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { GFMap } from './components/GFMap';
+// import { GroundMap } from './components/GroundMap';
+// import { GroundMapp } from './components/GroundMapp';
+// import { TestGround } from './components/TestGround'
+
+function App() {
+  return (
+      <div className="App">
+        <GFMap></GFMap>
+        {/* <GroundMap></GroundMap> */}
+        {/* <GroundMapp></GroundMapp> */}
+        {/* <TestGround></TestGround> */}
+      </div>
+  )
+}
+
+export default App;
