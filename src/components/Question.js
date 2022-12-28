a935d0a836342b7e4c42b16cb06e536554f2bb12
arricodyanto <arricohandyanto@gmail.com>
2022-12-29 04:55:31 +0700

    complete the tenant's legend

---
+++ src/components/Question.js
@@ -0,0 +1,37 @@
+// // how to display spesific array that same with element id?
+// myArray.find(x => x.id === '45').foo;
+
+
+// myArray.findIndex(x => x.id === '45');
+
+
+// myArray.filter(x => x.id === '45');
+
+
+// myArray.filter(x => x.id === '45').map(x => x.foo);
+
+//how to send value from element?
+// function add() {
+//   var a3 = document.getElementById('a').value;
+//   document.getElementById("greeting").textContent += " " + a3;    
+// }
+
+// <form>
+//   a: <input type="number" name="a" id="a"><br>
+
+//   <button type="button" onclick="add()">Add</button>
+//   <p id="greeting">Greetings</p>
+// </form>
+
+// how to get value of element id?
+// if(document.getElementById("cost").value == "" || isNaN(document.getElementById("cost").value)
+
+
+// <input type="text" id="cost" name="cost" value="" onchange="fixOrder()">
+
+// how to get element id?
+
+
+
+
+
--- src/components/TestGesture.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const TestGesture = () => {
-  return (
-    <div></div>
-  )
-}
